9e6f859192478b143130ae6e682945a5701c57a1
BijanProgrammer <bijaneisapour@gmail.com>
2024-11-23 09:50:15 +0330

    [ADD] setState callback

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import { useState } from "react";
 
 const numbers = [1, 2, 3];
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const clickHandler = () => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
+
   return (
     <>
+      <button onClick={clickHandler}>count: {count}</button>
       {numbers.map((x) => (
         <div key={x}>{x}</div>
       ))}
